0444d6ad123ee25d798caa5a6e1a86974b942856
mcastae813 <martinjosecastaneda1@gmail.com>
2024-10-24 21:07:48 -0500

    Paste

--- LinearInputControl/index.ts
+++ LinearInputControl/index.ts
@@ -9,7 +9,11 @@ export class LinearInputControl implements ComponentFramework.StandardControl<II
     {
 
     }
-
+    public refreshData(evt: Event): void {
+        this._value = this.inputElement.value as any as number;
+        this.labelElement.innerHTML = this.inputElement.value;
+        this._notifyOutputChanged();
+    }
     /**
      * Used to initialize the control instance. Controls can kick off remote server calls and other initialization actions here.
      * Data-set values are not initialized here, use updateView.
@@ -18,11 +22,47 @@ export class LinearInputControl implements ComponentFramework.StandardControl<II
      * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
      * @param container If a control is marked control-type='standard', it will receive an empty div element within which it can render its content.
      */
-    public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container:HTMLDivElement): void
-    {
-        // Add control initialization code
-    }
-
+    public init(
+        context: ComponentFramework.Context<IInputs>,
+        notifyOutputChanged: () => void,
+        state: ComponentFramework.Dictionary,
+        container: HTMLDivElement
+        ): void {
+           // Add control initialization code
+        this._context = context;
+        this._container = document.createElement("div");
+        this._notifyOutputChanged = notifyOutputChanged;
+        this._refreshData = this.refreshData.bind(this);
+        // creating HTML elements for the input type range and binding it to the function which 
+        // refreshes the control data
+        this.inputElement = document.createElement("input");
+        this.inputElement.setAttribute("type", "range");
+        this.inputElement.addEventListener("input", this._refreshData);
+        //setting the max and min values for the control.
+        this.inputElement.setAttribute("min", "1");
+        this.inputElement.setAttribute("max", "1000");
+        this.inputElement.setAttribute("class", "linearslider");
+        this.inputElement.setAttribute("id", "linearrangeinput");
+        // creating a HTML label element that shows the value that is set on the linear range control
+        this.labelElement = document.createElement("label");
+        this.labelElement.setAttribute("class", "LinearRangeLabel");
+        this.labelElement.setAttribute("id", "lrclabel");
+        // retrieving the latest value from the control and setting it to the HTMl elements.
+        this._value = context.parameters.controlValue.raw!;
+        this.inputElement.setAttribute(
+            "value",
+            context.parameters.controlValue.formatted
+            ? context.parameters.controlValue.formatted
+            : "0"
+        );
+        this.labelElement.innerHTML = context.parameters.controlValue.formatted
+            ? context.parameters.controlValue.formatted
+            : "0";
+        // appending the HTML elements to the control's HTML container element.
+        this._container.appendChild(this.inputElement);
+        this._container.appendChild(this.labelElement);
+        container.appendChild(this._container);
+}
 
     /**
      * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
